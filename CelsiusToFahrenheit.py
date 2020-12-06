52f23de4e5e281839da1b2bd4303d9ccf6e9f3e1
Franklyn Sancho <69439514+Franklyn-Sancho@users.noreply.github.com>
2020-12-06 12:11:36 -0500

    Celsius para Fahrenheit

---
+++ CelsiusToFahrenheit.py
@@ -0,0 +1,5 @@
+#Converter a temperatura de celcius para Fahrenheit
+
+c = float(input('Informe a temperatura em Celsius: '))
+f = 9 * c/ 5 + 32
+print('A temperatura de {}*c corresponde a {}*f'.format(c, f))
